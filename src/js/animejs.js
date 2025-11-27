f3b3f8fa8d2677dbc7952e343e3006ec25d42560
jaydenF08 <142918504+jaydenF08@users.noreply.github.com>
2025-11-27 13:22:55 -0500

    anime_ligne

--- src/js/animejs.js
+++ src/js/animejs.js
@@ -1,6 +1,13 @@
 import { animate, utils } from 'animejs';
 
+const min = utils.random(0.1, 0.4);
+const max = utils.random(0.7, 1);
+
 animate(".line1", {
-  opacity: () => utils.random(0, 1, 2),
-  loop: true,
+  opacity: [min, max],
+  duration: utils.random(1000, 2000),
+  direction: "alternate",
+  easing: "easeInOutSine",
+  loop: true
 });
+
