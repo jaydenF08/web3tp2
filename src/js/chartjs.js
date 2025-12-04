9027a6eb7356cf86a9a2ae2073bf22993c8583a5
jaydenF08 <142918504+jaydenF08@users.noreply.github.com>
2025-12-04 18:54:30 -0500

    text

--- src/js/chartjs.js
+++ src/js/chartjs.js
@@ -1,12 +1,16 @@
 import { animate, splitText, stagger, utils } from "https://esm.sh/animejs";
 
+
 const { chars } = splitText("p", {
   chars: { wrap: "clip" }
 });
+
 
 animate(chars, {
   x: ["100%", "0%"],
   duration: 10,
   delay: stagger(150),
+  loopDelay: 5000,
   loop: true
 });
+
